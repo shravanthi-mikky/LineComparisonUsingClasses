ee0cfee944df06523b4720826468647f15e8194b
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-04-23 13:47:37 +0530

    Using equals method

--- LineComparison-Using-Classes/Program.cs
+++ LineComparison-Using-Classes/Program.cs
@@ -1,20 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LineComparison_Using_Classes;
-/*
-Console.WriteLine("Welcome To Line COmparison Using Classes and Methods!");
-Console.WriteLine("Please Enter x1 and y1 coordinates ");
-var x1 = Convert.ToDouble(Console.ReadLine());
-var y1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Please Enter x2 and y2 coordinates ");
-var x2 = Convert.ToDouble(Console.ReadLine());
-var y2 = Convert.ToDouble(Console.ReadLine());
-*/
-
+//creating objects
 UC1 uc1 = new();
-
+uc2_CompareUsingEquals equals = new();
+//taking input for first line coordinates
 double x1 = uc1.Input();
 double x2 = uc1.Input();
 double y1 = uc1.Input();
 double y2 = uc1.Input();
+//distance of line 1
 double finalResult = uc1.Distance(x1, x2, y1, y2);
 Console.WriteLine("Distance between {0},{1} and {2},{3} is {4:F}", x1, y1, x2, y2, finalResult);
+//taking input for second line coordinates
+double p = uc1.Input();
+double r = uc1.Input();
+double q = uc1.Input();
+double s = uc1.Input();
+//distance of line 2
+double finalResult2 = uc1.Distance(p, r, q, s);
+Console.WriteLine("Distance between {0},{1} and {2},{3} is {4:F}", p, q, r, s, finalResult2);
+//calling method from uc2 to compare lenthgs using Equals
+equals.ComparisionByEquals(finalResult, finalResult2);
---
+++ LineComparison-Using-Classes/uc2_CompareUsingEquals.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison_Using_Classes
+{
+    public class uc2_CompareUsingEquals
+    {
+        public void ComparisionByEquals(double finalResult, double finalResult2)
+        {
+            bool status = finalResult.Equals(finalResult2);
+            if (status)
+                Console.WriteLine("{0} is equal to {1}", finalResult, finalResult2);
+            else
+                Console.WriteLine("{0} is not equal to {1}", finalResult, finalResult2);
+        }
+    }
+}
